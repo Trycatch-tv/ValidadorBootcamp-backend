eb694fe14e84bfbb750a23d59239052ddee1a53d
Laura Velez <velezlaura15@gmail.com>
2023-11-29 23:07:36 -0500

    Se agrega signin en Users con clases y swagger

--- src/controllers/users/users.controller.ts
+++ src/controllers/users/users.controller.ts
@@ -2,21 +2,46 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SignupDto } from 'src/dtos/users/signup.dto';
 import { SignupResponse } from 'src/responses/users/signup.response';
 import { UsersService } from 'src/services/users/users.service';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SigninResponse } from 'src/responses/users/signin.response';
 
-@Controller()
+@ApiTags('Users')
+@Controller('users')
 export class UsersController {
-  private readonly usersService: UsersService;
-  constructor(usersService: UsersService) {
+  
+  constructor(private readonly usersService: UsersService) {
     this.usersService = usersService;
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'Returns an array of users',
+    type: [SignupResponse]
+  })
   @Get('/')
-  healthCheck() {
-    return 'ok';
+  getUsers(): Promise<SignupResponse[]> {
+    return this.usersService.getUsers();
+  }
+  
+  @ApiBody({ type: SignupDto })
+  @ApiResponse({
+    status: 200,
+    description: 'User created',
+    type: SignupResponse,
+  })
+  @Post('signup')
+  async signUp(@Body() signupDto: SignupDto): Promise<SignupResponse> {
+    return await this.usersService.signup(signupDto);
   }
 
-  @Post('signup')
-  async signup(@Body() signupDto: SignupDto): Promise<SignupResponse> {
-    return await this.usersService.signup(signupDto);
+  @ApiBody({ type: SignupDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Sign in successful',
+    type: SigninResponse,
+  })
+  @Get('signin')
+  async signIn(@Body() signupDto: SignupDto): Promise<SigninResponse> {
+    return await this.usersService.signin(signupDto);
   }
 }
--- src/dtos/users/signup.dto.ts
+++ src/dtos/users/signup.dto.ts
@@ -1,6 +1,13 @@
-export interface SignupDto {
+import { ApiProperty } from "@nestjs/swagger";
+
+export class SignupDto {
+  id: string;
+  @ApiProperty({ example: "John", description: 'User Name' })
   first_name: string;
+  @ApiProperty({ example: "Doe", description: 'User Last Name' })
   last_name: string;
-  email: string;
+  @ApiProperty({ example: "johndoe@example.com", description: 'User Email' })
+  email: string;  
+  @ApiProperty({ example: "Password123", description: 'User Role' })
   password: string;
 }
