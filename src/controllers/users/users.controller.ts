2f8c24556bdf5b588c127b81a68a7e86e607e6fa
orliluq <orlibet@gmail.com>
2023-12-08 16:36:29 -0400

    jest refactorizado

--- src/controllers/users/users.controller.ts
+++ src/controllers/users/users.controller.ts
@@ -1,22 +1,66 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { SignupDto } from 'src/dtos/users/signup.dto';
-import { SignupResponse } from 'src/responses/users/signup.response';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Body, Controller, Get, Post, Response, HttpStatus } from '@nestjs/common';
 import { UsersService } from 'src/services/users/users.service';
+import { User } from 'src/models/user/user.entity';
+import { SignupService } from 'src/services/signup/signup.service';
+
+export interface SignupDto {
+  name: string;
+  email: string;
+  password: string;
+  // Add the missing properties from the User interface
+  id: string;
+  role: string;
+  is_active: boolean;
+  // TODO: Add any other missing properties
+}
+
+interface SignupResponse {
+  // Existing properties
+  name: string;
+  email: string;
+  id: string;
+}
 
 @Controller()
 export class UsersController {
-  private readonly usersService: UsersService;
-  constructor(usersService: UsersService) {
-    this.usersService = usersService;
-  }
+  constructor(
+    private readonly signupService: SignupService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Get('/')
-  healthCheck() {
+  healthCheck(): string {
     return 'ok';
   }
 
   @Post('signup')
-  async signup(@Body() signupDto: SignupDto): Promise<SignupResponse> {
-    return await this.usersService.signup(signupDto);
+async signup(@Body() signupDto: SignupDto, @Response() res: Response): Promise<any> {
+  try {
+    const user: User = {
+      name: signupDto.name,
+      email: signupDto.email,
+      password: signupDto.password,
+      // Set any default values for missing properties
+      id: '',
+      role: 'user',
+      is_active: false,
+      setDefaultRole: function (): Promise<void> {
+        throw new Error('Function not implemented.');
+      },
+      first_name: '',
+      last_name: '',
+      created_at: undefined,
+      updated_at: undefined
+    };
+    const registeredUser = await this.signupService.signup(user);
+    const response: SignupResponse = {
+      name: registeredUser.name,
+      email: registeredUser.email,
+      id: registeredUser.id,
+    };
+    return res.status(HttpStatus.CREATED).send(response);
+  } catch (error) {
+    return res.status(HttpStatus.BAD_REQUEST).send({ errors: error.errors });
   }
-}
+}};
--- test/controllers/users/users.controller.spec.ts
+++ test/controllers/users/users.controller.spec.ts
@@ -1,20 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from '../../../src/controllers/users/users.controller';
+import { UsersService } from '../../../src/services/users/users.service';
 
-describe('UsersController', () => {
+describe('Users', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
+
+      
+    controllers: [UsersController],
+      providers: [UsersService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('UsersController', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+
+    it('should handle signup requests successfully', async () => {
+      const user = {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: 'secret',
+      };
+      const expectedResponse = {
+        name: user.name,
+        email: user.email,
+        id: '',
+      };
+
+      jest.spyOn(service, 'signup').mockImplementation(() => Promise.resolve(expectedResponse));
+
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      await controller.signup(res, user);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.send).toHaveBeenCalledWith(expectedResponse);
+    });
+
+    it('should handle signup requests with invalid data', async () => {
+      const user = {
+        name: '',
+        email: 'invalid-email',
+        password: '',
+      };
+      const expectedErrors = {
+        name: ['El nombre no puede estar vacío'],
+        email: ['La dirección de correo electrónico no es válida'],
+        password: ['La contraseña no puede estar vacío'],
+      };
+
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      await controller.signup(res, user);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.send).toHaveBeenCalledWith({ errors: expectedErrors });
+    });
+  });
+
+  describe('UsersService', () => {
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
   });
 });
-export { UsersController };
-
