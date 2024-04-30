758bf1b138f740be9c4e0ec815875711d60e0e74
J4F3ET <jslopezz@udistrital.edu.co>
2024-04-29 21:58:33 -0500

    fix: :construction: Remover console.log

--- src/middlewares/users/authorization.middleware.ts
+++ src/middlewares/users/authorization.middleware.ts
@@ -1,10 +1,9 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class UserAuthorizationMiddleware implements NestMiddleware {
-    use(req: Request, res: Response, next: NextFunction) {
-        console.log('UserAuthorizationMiddleware');
+    use(req: Request, res: Response, next: () => void) {
+        //  Logic to check if the user is authorized
         next();
     }
 }
