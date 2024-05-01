b22270f586acf5327db79285cfbb241a0613d40f
J4F3ET <jslopezz@udistrital.edu.co>
2024-04-30 19:01:46 -0500

    feat: :sparkles: Agregar UserAuthorizationMiddleware

--- src/middlewares/users/authorization.middleware.ts
+++ src/middlewares/users/authorization.middleware.ts
@@ -1,9 +1,28 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserAuthorizationMiddleware implements NestMiddleware {
-    use(req: Request, res: Response, next: () => void) {
-        //  Logic to check if the user is authorized
-        next();
+  constructor(private readonly jwtService: JwtService) {}
+  async use(req: Request, res: Response, next: () => void) {
+    const token = this.extractTokenFromHeader(req);
+    if (!(await token)) {
+      throw new UnauthorizedException('No token provided');
     }
+    const payload = this.jwtService.decode(await token);
+    if (!payload) {
+      throw new UnauthorizedException('Invalid token');
+    }
+    next();
+  }
+  private async extractTokenFromHeader(
+    req: Request,
+  ): Promise<string | undefined> {
+    const [type, token] = req.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
 }
