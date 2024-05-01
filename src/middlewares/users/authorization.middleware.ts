2dd33991d207ef4d0f3141d003f6af5e92afbe82
J4F3ET <jslopezz@udistrital.edu.co>
2024-04-30 22:17:05 -0500

    feat: :sparkles: Agregando JWT a middleware de userAuthorization

--- src/middlewares/users/authorization.middleware.ts
+++ src/middlewares/users/authorization.middleware.ts
@@ -1,24 +1,32 @@
 import {
   Injectable,
+  InternalServerErrorException,
   NestMiddleware,
   UnauthorizedException,
 } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { JsonWebTokenError, JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserAuthorizationMiddleware implements NestMiddleware {
   constructor(private readonly jwtService: JwtService) {}
+
   async use(req: Request, res: Response, next: () => void) {
-    const token = this.extractTokenFromHeader(req);
-    if (!(await token)) {
-      throw new UnauthorizedException('No token provided');
+    const token = await this.extractTokenFromHeader(req);
+    if (!(token)) throw new UnauthorizedException('No token provided');
+    try {
+        await this.jwtService.verifyAsync(token, {
+            secret: process.env.JWT_SECRET,
+            algorithms: ['HS256'],
+        });
+        next();
+    } catch (err) {
+        if(err instanceof UnauthorizedException || err instanceof JsonWebTokenError)
+            throw new UnauthorizedException(err.message);
+        else
+            throw new InternalServerErrorException(err.message);
     }
-    const payload = this.jwtService.decode(await token);
-    if (!payload) {
-      throw new UnauthorizedException('Invalid token');
-    }
-    next();
   }
+
   private async extractTokenFromHeader(
     req: Request,
   ): Promise<string | undefined> {
