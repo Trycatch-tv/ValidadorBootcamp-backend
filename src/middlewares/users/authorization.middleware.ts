eb795d9b848219eddae060a5f77a4b4a315766fa
J4F3ET <jslopezz@udistrital.edu.co>
2024-06-05 22:18:06 -0500

    refactor: :construction:  Update UserAuthorizationMiddleware to use AuthorizationAsyncToken function

--- src/middlewares/users/authorization.middleware.ts
+++ src/middlewares/users/authorization.middleware.ts
@@ -4,28 +4,27 @@ import {
   NestMiddleware,
   UnauthorizedException,
 } from '@nestjs/common';
-import { JsonWebTokenError, JwtService } from '@nestjs/jwt';
-import { extractTokenFromHeader } from 'src/utils/JWT/jwt.utils';
+import { JwtService } from '@nestjs/jwt';
+import { AuthorizationAsyncToken } from 'src/utils/JWT/jwt.utils';
 
 @Injectable()
 export class UserAuthorizationMiddleware implements NestMiddleware {
   constructor(private readonly jwtService: JwtService) {}
 
   async use(req: Request, res: Response, next: () => void) {
-    const token = await extractTokenFromHeader(req);
-    if (!(token)) throw new UnauthorizedException('No token provided');
     try {
-        await this.jwtService.verifyAsync(token, {
-            secret: process.env.JWT_SECRET,
-            algorithms: ['HS256'],
-        });
-        next();
+      // AuthorizationAsyncToken returns the payload of the token
+      await AuthorizationAsyncToken(this.jwtService,req);
+      next();
     } catch (err) {
-        if(err instanceof UnauthorizedException || err instanceof JsonWebTokenError)
-            throw new UnauthorizedException(err.message);
-        else
-            throw new InternalServerErrorException(err.message);
+      if (
+        err.name === 'UnauthorizedException' ||
+        err.name === 'JsonWebTokenError'
+      ) {
+        throw new UnauthorizedException(err.message);
+      } else {
+        throw new InternalServerErrorException(err.message);
+      }
     }
   }
-
 }
